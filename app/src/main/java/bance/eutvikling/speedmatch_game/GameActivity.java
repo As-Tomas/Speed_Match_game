242432ea294a9fce4ed960641eeca01fd7ed912e
Bance <kalianasu@gmail.com>
2022-06-22 23:43:42 +0200

    +basic buttons yes no functionality, changes frames

--- app/src/main/java/bance/eutvikling/speedmatch_game/GameActivity.java
+++ app/src/main/java/bance/eutvikling/speedmatch_game/GameActivity.java
@@ -1,11 +1,20 @@
 package bance.eutvikling.speedmatch_game;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
     private int time;
@@ -14,6 +23,15 @@ public class GameActivity extends AppCompatActivity {
     private SquareFragment squareFragment = new SquareFragment();
     private CircleFragment circleFragment = new CircleFragment();
 
+    private ArrayList <Fragment> ShapesFragments = new ArrayList();
+
+    private Random random = new Random();
+    private int min = 0;
+    private int max = 2;
+
+    private int shapeNumber;
+    private int previousShape;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,15 +39,48 @@ public class GameActivity extends AppCompatActivity {
 
         //play game  / return points
 
-        points = 12;
 
+        ShapesFragments.add(triangleFragment);
+        ShapesFragments.add(squareFragment);
+        ShapesFragments.add(circleFragment);
 
-        setCurrentFragment(triangleFragment);
+        shapeNumber = random.nextInt(max - min + 1) + min;
+        previousShape = shapeNumber;
 
-//        Intent retIntent=new Intent();
-//        retIntent.putExtra("Points", points);
-//        setResult(Activity.RESULT_OK,retIntent);
-//        finish();
+        setCurrentFragment(ShapesFragments.get(shapeNumber));
+
+        Button btnNo = findViewById(R.id.btnNo);
+        btnNo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(previousShape != shapeNumber){
+                    points++;
+                }
+                previousShape = shapeNumber;
+                shapeNumber = random.nextInt(max - min + 1) + min;
+                setCurrentFragment(ShapesFragments.get(shapeNumber));
+            }
+        });
+
+        Button btnYes = findViewById(R.id.btnYes);
+        btnYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(previousShape == shapeNumber){
+                    points++;
+                }
+                previousShape = shapeNumber;
+                shapeNumber = random.nextInt(max - min + 1) + min;
+                setCurrentFragment(ShapesFragments.get(shapeNumber));
+            }
+        });
+
+//        if(points > 10){
+//            Intent retIntent=new Intent();
+//            retIntent.putExtra("Points", points);
+//            setResult(Activity.RESULT_OK,retIntent);
+//            finish();
+//        }
     }
 
     public void setCurrentFragment(Fragment fragment){
