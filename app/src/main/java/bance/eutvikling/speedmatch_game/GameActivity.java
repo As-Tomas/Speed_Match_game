1fe799414d885a23af1eda675a38599e4be27207
Bance <kalianasu@gmail.com>
2022-06-25 14:31:55 +0200

    + CountDownTimer

--- app/src/main/java/bance/eutvikling/speedmatch_game/GameActivity.java
+++ app/src/main/java/bance/eutvikling/speedmatch_game/GameActivity.java
@@ -8,8 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -32,6 +34,9 @@ public class GameActivity extends AppCompatActivity {
 
     private int shapeNumber;
     private int previousShape;
+
+    CountDownTimer cTimer = null;
+    TextView mTimeField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,7 +65,6 @@ public class GameActivity extends AppCompatActivity {
                 previousShape = shapeNumber;
                 shapeNumber = random.nextInt(max - min + 1) + min;
                 setCurrentFragment(ShapesFragments.get(shapeNumber));
-                checkResult();
             }
         });
 
@@ -74,9 +78,11 @@ public class GameActivity extends AppCompatActivity {
                 previousShape = shapeNumber;
                 shapeNumber = random.nextInt(max - min + 1) + min;
                 setCurrentFragment(ShapesFragments.get(shapeNumber));
-                checkResult();
             }
         });
+
+        mTimeField = findViewById(R.id.time);
+        startTimer();
 
     }
 
@@ -89,13 +95,35 @@ public class GameActivity extends AppCompatActivity {
                 .commit();
     }
 
-    public void checkResult(){
-        Toast.makeText(this," Points: "+ points, Toast.LENGTH_SHORT).show();
-        if(points > 2){
-            Intent retIntent=new Intent();
-            retIntent.putExtra("Points", points);
-            setResult(Activity.RESULT_OK,retIntent);
-            finish();
-        }
+    //start timer function
+    void startTimer() {
+        cTimer = new CountDownTimer(60000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                mTimeField.setText("seconds remaining: " + millisUntilFinished / 1000);
+
+            }
+            public void onFinish() {
+                mTimeField.setText("done!");
+                cancelTimer();
+                //TODO add finish fragment, her should display finish fragment
+                Intent retIntent=new Intent();
+                retIntent.putExtra("Points", points);
+                setResult(Activity.RESULT_OK,retIntent);
+                finish();
+            }
+        };
+        cTimer.start();
+    }
+
+    //cancel timer
+    void cancelTimer() {
+        if(cTimer!=null)
+            cTimer.cancel();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cTimer.cancel();
     }
 }
