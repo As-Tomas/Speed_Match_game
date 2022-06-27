a408df3c781b4651bc57b1865351d7d788d12356
Bance <kalianasu@gmail.com>
2022-06-27 10:49:55 +0200

    + Save / read set to sharedPreferences

--- app/src/main/java/bance/eutvikling/speedmatch_game/MainActivity.java
+++ app/src/main/java/bance/eutvikling/speedmatch_game/MainActivity.java
@@ -8,18 +8,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     // Game: different shapes slides from right to left, stops in the middle, player must chose buttons
     // yes or no to answer  Does this symbol match the previous symbol.
     // score calculates by amount of right answers in a given time.
 
-    ActivityResultLauncher<Intent> game_louncher;
+    ActivityResultLauncher<Intent> game_launcher;
+    public ArrayList<String> results;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        game_louncher=registerForActivityResult(
+        game_launcher =registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 new ActivityResultCallback<ActivityResult>() {
                     @Override
@@ -35,7 +41,11 @@ public class MainActivity extends AppCompatActivity {
                         if(result.getResultCode()== Activity.RESULT_OK){
                             setContentView(R.layout.activity_main);
                             Intent intent=result.getData();
+
+                            //Save result and display it
                             float points=intent.getFloatExtra("Points",0);
+                            saveDB(points);
+//TODO display as list to top 10 scores
                             TextView resultPoints = findViewById(R.id.result);
                             resultPoints.setText("Points :  "+points);
                         }
@@ -51,9 +61,38 @@ public class MainActivity extends AppCompatActivity {
 
                 //Intent i=new Intent( , GameActivity.class);
                 Intent intent = new Intent(MainActivity.this, GameActivity.class);
-                game_louncher.launch(intent);
+                game_launcher.launch(intent);
             }
         });
     }
+
+    public void saveDB(float points){
+        SharedPreferences sharedPrefs = this.getSharedPreferences("speedMatchPoints",this.MODE_PRIVATE);
+        SharedPreferences.Editor edit=sharedPrefs.edit();
+
+        if(results == null){
+            results = new ArrayList();
+        }
+
+        results.add(String.valueOf(points));
+
+        Set<String> set  = new HashSet<String>();
+        set.addAll(results);
+
+        edit.putStringSet("Points", set);
+    }
+
+    public void readDB(){
+        SharedPreferences sharedPrefs = this.getSharedPreferences("speedMatchPoints",this.MODE_PRIVATE);
+        Set<String> set = sharedPrefs.getStringSet("Points", null);
+        if(results == null){
+            results=new ArrayList<String>(set);
+        } else {
+            results.clear();
+            results=new ArrayList<String>(set);
+        }
+
+    }
+
 
 }
