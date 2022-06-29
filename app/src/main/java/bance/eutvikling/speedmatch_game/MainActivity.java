2720a47291c8e4e3f686292178b406db32a62f8c
Bance <kalianasu@gmail.com>
2022-06-29 10:11:55 +0200

    Bug fix sharedPreferences

--- app/src/main/java/bance/eutvikling/speedmatch_game/MainActivity.java
+++ app/src/main/java/bance/eutvikling/speedmatch_game/MainActivity.java
@@ -12,12 +12,14 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     // Game: different shapes slides from right to left, stops in the middle, player must chose buttons
@@ -26,12 +28,32 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityResultLauncher<Intent> game_launcher;
     public ArrayList<String> results;
+    ListView listView;
+
+    String[] temp ={"temp", "arr"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            readDB();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if(results != null){
+
+        }
+        listView = findViewById(R.id.list);
+        ArrayAdapter adapter=new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                temp
+        );
+
 
         game_launcher =registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
@@ -45,7 +67,10 @@ public class MainActivity extends AppCompatActivity {
                             //Save result and display it
                             float points=intent.getFloatExtra("Points",0);
                             saveDB(points);
-//TODO display as list to top 10 scores
+//TODO display as list to top 10
+
+
+
                             TextView resultPoints = findViewById(R.id.result);
                             resultPoints.setText("Points :  "+points);
                         }
@@ -67,8 +92,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void saveDB(float points){
-        SharedPreferences sharedPrefs = this.getSharedPreferences("speedMatchPoints",this.MODE_PRIVATE);
-        SharedPreferences.Editor edit=sharedPrefs.edit();
+        SharedPreferences sharedPrefs = getSharedPreferences("speedMatchPoints", MODE_PRIVATE);
+        SharedPreferences.Editor editor=sharedPrefs.edit();
 
         if(results == null){
             results = new ArrayList();
@@ -79,16 +104,19 @@ public class MainActivity extends AppCompatActivity {
         Set<String> set  = new HashSet<String>();
         set.addAll(results);
 
-        edit.putStringSet("Points", set);
+        editor.putStringSet("Points", set);
+        editor.commit();
     }
 
     public void readDB(){
-        SharedPreferences sharedPrefs = this.getSharedPreferences("speedMatchPoints",this.MODE_PRIVATE);
+        SharedPreferences sharedPrefs = getSharedPreferences("speedMatchPoints", MODE_PRIVATE);
         Set<String> set = sharedPrefs.getStringSet("Points", null);
-        if(results == null){
-            results=new ArrayList<String>(set);
+        if(results == null && set.size() == 0){
+            results=new ArrayList<String>();
         } else {
-            results.clear();
+            if(results != null ) {
+                results.clear();
+            }
             results=new ArrayList<String>(set);
         }
 
